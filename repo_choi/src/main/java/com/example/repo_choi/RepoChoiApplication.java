5200c289c27a93476bca78bc18cd560a9cf0bcd2
choi <choi@ch>
2025-05-07 11:38:39 +0900

    initial

---
+++ repo_choi/src/main/java/com/example/repo_choi/RepoChoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.repo_choi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoChoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoChoiApplication.class, args);
+	}
+
+}
